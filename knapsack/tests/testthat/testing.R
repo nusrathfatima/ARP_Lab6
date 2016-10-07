7bc31d8009896624eb17c6450e21f29f0c7cc3ac
ejarkm <ejarkm@gmail.com>
2016-10-07 11:50:55 +0200

    Vignette updated

--- knapsack/tests/testthat/testing.R
+++ knapsack/tests/testthat/testing.R
@@ -1,11 +1,11 @@
 # Test
+library(knapsack)
+library(testthat)
+
 set.seed(42)
 n <- 2000
 knapsack_objects <- data.frame(w=sample(1:4000, size = n, replace = TRUE), 
                                v=runif(n = n, 0, 10000))
-
-library(knapsack)
-library(testthat)
 
 test_that("testing function brute_force_knapsack", {
   # true values
